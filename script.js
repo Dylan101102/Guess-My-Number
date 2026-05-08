eb096b875e832d99164bf02da29d90b007b62b15
Dylan Roach <dylan.roach@flhealth.gov>
2026-05-08 16:35:23 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+// Defining secret number.
+const secret_number = Math.floor(Math.random() * 20) + 1; // Adding one shifts the random value range from 1 (inclusive) to 21 (exclusive).
+document.querySelector('.number').textContent = secret_number; // Will be shown while developing.
+
+// Set up the score in the code -> better to not rely on the DOM to hold the data.
+let score = 20;
+
+// Once the "Check" button is clicked, I want to do a score comparison.
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); // Inputted values are strings at first.
+
+  if (!guess) {
+    // If the user doesn't guess anything, it is at first an empty string, which is converted to 0 when converted to a number.
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === secret_number) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > secret_number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '😔 Game over. You lost.';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secret_number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '😔 Game over. You lost.';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
